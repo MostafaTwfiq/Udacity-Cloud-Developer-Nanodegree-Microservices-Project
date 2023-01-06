f7ddb972adf0bcceba2c0794b22db8387d3226e0
MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
2023-01-06 13:20:43 +0200

    Test

---
+++ create_pods.bat
@@ -0,0 +1,4 @@
+kubectl apply -f ./udagram-api-feed/deployment.yaml
+kubectl apply -f ./udagram-api-user/deployment.yaml
+kubectl apply -f ./udagram-frontend/deployment.yaml
+kubectl apply -f ./udagram-reverseproxy/deployment.yaml
---
+++ delete_pods.bat
@@ -0,0 +1,4 @@
+kubectl delete deployment backend-feed
+kubectl delete deployment backend-user
+kubectl delete deployment frontend
+kubectl delete deployment reverseproxy
